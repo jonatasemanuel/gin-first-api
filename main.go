1a7818835a1dcb07c405c9c5881ca21beda267f3
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-12-18 20:15:22 -0300

    first go-gin api

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"gin-first-api/controllers"
+	"gin-first-api/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	models.ConnectDatabase()
+
+	r.GET("/books", controllers.FindBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	err := r.Run()
+	if err != nil {
+		return
+	}
+}
